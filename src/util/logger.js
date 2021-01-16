0e3b3ca6cf4be39571afdd7b6db0c859f5034a8d
Arif Ishtiaque <arifistiak.133@gmail.com>
2021-01-16 23:19:41 +0600

    logger implementetion

---
+++ src/util/logger.js
@@ -0,0 +1,46 @@
+const { createLogger, format, transports, Logger, info } = require("winston");
+
+function formatParams(info) {
+  const { timestamp, level, message, ...args } = info;
+  const ts = timestamp.slice(0, 19).replace("T", " ");
+
+  return `${ts} ${level}: ${message} ${
+    Object.keys(args).length ? JSON.stringify(args, "") : ""
+  }`;
+}
+
+// dev format
+const devFormat = format.combine(
+  format.colorize(),
+  format.timestamp(),
+  format.align(),
+  format.printf(formatParams)
+);
+
+//prod format
+const prodFormat = format.combine(
+  format.timestamp(),
+  format.align(),
+  format.printf(formatParams) //config later
+);
+
+let logger = null;
+if (process.env.NODE_ENV === "production") {
+  logger = createLogger({
+    level: 'info',
+    format: prodFormat,
+    transports: [
+      new transports.File({ filename: "logs/error.log", level: "error" }),
+      new transports.File({ filename: "logs/combined" }),
+    ],
+  });
+} else {
+  logger = createLogger({
+    level: 'info',
+    format: devFormat,
+    transports: [new transports.Console()],
+  });
+}
+
+
+module.exports = logger;
